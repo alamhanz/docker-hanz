3ee94f372de3d5a7eb04efd26fa508e602418dcc
alamhanz <alamhanz@gmail.com>
2022-07-17 15:49:29 +0700

    [Alam] structure updates

---
+++ data-analysis-base/py37-0722/install.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+echo "apt get and req"
+apt-get update
+apt-get upgrade
+apt-get install -yq $(grep -vE "^\s*#" apt.txt  | tr "\n" " ")
+python -m pip install pip==22.1.2
+pip install -U -r requirements.txt 
